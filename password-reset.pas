ef7b44d0c57349f4e33943ae5cbcd1114bf20dbc
Perry van den Hondel <perry.vandenhondel@gmail.com>
2015-07-21 14:49:08 +0200

    Started dev branch.

---
+++ password-reset.pas
@@ -0,0 +1,20 @@
+program UDatabaseDsnTest;
+
+
+{$MODE OBJFPC} 	// Directive to be used for creating classes
+{$M+} 			// Directive that allows class constructors and destructor's
+{$H+}
+
+
+
+uses
+	SqlDb,
+
+
+	
+	
+begin
+	ProgInit();
+	ProgRun();
+	ProgDone();
+end.
